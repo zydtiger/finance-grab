2deb7fcee676177884d737a1ff4318841303cf5f
tigerding <zhiyuanding01@gmail.com>
2025-01-10 23:11:34 +0800

    feat: bind theme to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
+/**
+ * The application entry point that creates the React root.
+ * @author tigerding <zhiyuanding01@gmail.com>
+ */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ThemeProvider from "./theme/ThemeProvider";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
